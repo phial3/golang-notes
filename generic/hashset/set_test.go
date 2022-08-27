05353ca52b4b557a2bb08335f83fa65ad3fac299
gyf <gyf@travelsky.com.cn>
2022-08-27 20:46:06 +0800

    update generic

---
+++ generic/hashset/set_test.go
@@ -0,0 +1,84 @@
+package hashset_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	g "github.com/phial3/generic"
+	"github.com/phial3/generic/hashset"
+)
+
+func checkeq[K any](set *hashset.Set[K], get func(k K) bool, t *testing.T) {
+	set.Each(func(key K) {
+		if !get(key) {
+			t.Fatalf("value %v should be in the set", key)
+		}
+	})
+}
+
+func TestCrossCheck(t *testing.T) {
+	stdm := make(map[int]bool)
+	set := hashset.New[int](1, g.Equals[int], g.HashInt)
+
+	const nops = 1000
+	for i := 0; i < nops; i++ {
+		op := rand.Intn(2)
+		switch op {
+		case 0:
+			key := rand.Int()
+			stdm[key] = true
+			set.Put(key)
+		case 1:
+			var del int
+			for k := range stdm {
+				del = k
+				break
+			}
+			delete(stdm, del)
+			set.Remove(del)
+		}
+
+		checkeq(set, func(k int) bool {
+			_, ok := stdm[int(k)]
+			return ok
+		}, t)
+	}
+}
+
+func TestOf(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input []string
+	}{
+		{"init with several items", []string{"foo", "bar", "baz"}},
+		{"init without values", []string{}},
+	}
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			set := hashset.Of[string](10, g.Equals[string], g.HashString, tc.input...)
+
+			if len(tc.input) != set.Size() {
+				t.Fatalf("expected %d elements in set, got %d", len(tc.input), set.Size())
+			}
+			for _, val := range tc.input {
+				if !set.Has(val) {
+					t.Fatalf("expected to find val '%s' in set but did not", val)
+				}
+			}
+		})
+	}
+}
+
+func Example() {
+	set := hashset.New[string](3, g.Equals[string], g.HashString)
+	set.Put("foo")
+	set.Put("bar")
+	set.Put("baz")
+
+	fmt.Println(set.Has("foo"))
+	fmt.Println(set.Has("quux"))
+	// Output:
+	// true
+	// false
+}
---
+++ generic/list/list_test.go
@@ -0,0 +1,24 @@
+package list_test
+
+import (
+	"fmt"
+
+	"github.com/phial3/generic/list"
+)
+
+func Example() {
+	l := list.New[int]()
+	l.PushBack(0)
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	l.Front.Each(func(i int) {
+		fmt.Println(i)
+	})
+	// Output:
+	// 0
+	// 1
+	// 2
+	// 3
+}
